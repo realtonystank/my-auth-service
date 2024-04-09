9e141cbfdda3b69b42423c3b4d199529e5e61605
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2024-04-10 01:56:08 +0530

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    { ignores: ['**/dist/**', '**/node_modules/**'] },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            'no-console': 'error',
+        },
+    },
+);
