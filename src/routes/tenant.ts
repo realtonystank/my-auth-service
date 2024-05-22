2d3fb43adbcd76efae711f2b1445f57b0c1679cf
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2024-05-22 22:30:35 +0530

    add create tenant endpoint

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,8 +1,18 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { TenantController } from '../controllers/TenantController';
+import { TenantService } from '../services/TenantService';
+import { AppDataSource } from '../config/data-source';
+import { Tenant } from '../entity/Tenant';
+import { CreateTenantRequest } from '../types';
+import logger from '../config/logger';
 const router = express.Router();
 
-router.post('/', (req: Request, res: Response) => {
-    return res.status(201).json({});
-});
+const tenantRepository = AppDataSource.getRepository(Tenant);
+const tenantService = new TenantService(tenantRepository);
+const tenantController = new TenantController(tenantService, logger);
+
+router.post('/', (req: Request, res: Response, next: NextFunction) =>
+    tenantController.create(req as CreateTenantRequest, res, next),
+);
 
 export default router;
